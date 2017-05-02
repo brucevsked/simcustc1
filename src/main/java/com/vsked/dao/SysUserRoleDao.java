cdc83e34031ee416ff3844ba8c5f247c1ee971a7
chousheng <vsked@163.com>
2017-05-02 17:16:41 +0800

    userroledao junit test ok

--- src/main/java/com/vsked/dao/SysUserRoleDao.java
+++ src/main/java/com/vsked/dao/SysUserRoleDao.java
@@ -13,17 +13,11 @@ public interface SysUserRoleDao {
 	
 	public List<Map<String, Object>> getNoSysUserList(String srId);//查询未分配用户
 	
-	public List<Map<String, Object>> getHasSysRoleList(String srId);//查询已分配角色
+	public List<Map<String, Object>> getHasSysRoleList(String suId);//查询已分配角色
 	
-	public List<Map<String, Object>> getNoSysRoleList(String srId);//查询未分配角色
+	public List<Map<String, Object>> getNoSysRoleList(String suId);//查询未分配角色
 				
-	public int getSysUserRoleCount(Map<String,Object> m);
-	
-	public List<Map<String,Object>> getSysUserRoleList(Map<String,Object> m);
-	
 	public int sysUserRoleAdd(Map<String,Object> m);
-	
-	public int sysUserRoleEdit(Map<String,Object> m);
 	
 	public int sysUserRoleDelBySrId(String srId);//根据角色ID删除
 	
